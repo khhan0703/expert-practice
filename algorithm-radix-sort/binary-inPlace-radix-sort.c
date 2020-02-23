d4dbe3512bc2f9f790e462df0c1d3b9acc78fc4f
khhan0703 <kwanghun@gmail.com>
2020-02-23 10:13:47 +0900

    Binary in-plance radix sort r0

--- algorithm-radix-sort/binary-inPlace-radix-sort.c
+++ algorithm-radix-sort/binary-inPlace-radix-sort.c
@@ -7,7 +7,7 @@ note right
     // first, call RadixSort(data, MSB, 0, lengthOfData-1)
 end note
 
-partition Do-Radix-Sort {
+partition Do-Binary-Radix-Sort {
     :idxOf0 = startIdx
     idxOf1 = endIdx;
     repeat
@@ -16,7 +16,7 @@ partition Do-Radix-Sort {
         while (digit of data[idxOf1--] == 1) is (yes)
         endwhile (no)
         :swap(data[idxOf0], data[idxOf1]);
-    repeat while (idxOf0-1 == idxOf1) is (no)
+    repeat while (idxOf0 > idxOf1) is (no)
     -> yes;
     :startIdxOf0 = startIdx
     endIdxOf0 = idxOf0
@@ -36,3 +36,51 @@ note right
 end note
 @enduml
 */
+
+extern void print(int arr[], int n);
+extern void print4bit(int arr[], int n);
+int RADIX = 2;
+int radixSort(int data[], int n, int digit, int startIdx, int endIdx)
+{
+    //print(arr, n);
+    print4bit(data, n);
+    int mask = 1<<(digit-1);
+    register int idxOf0, idxOf1;
+    int tmpData;
+    while (1)
+    {
+        for (idxOf0 = startIdx;idxOf0 <= endIdx; idxOf0++)
+        { 
+            if((mask & data[idxOf0]) == mask) break;
+        }
+        for (idxOf1 = endIdx;idxOf0 >= startIdx; idxOf1--)
+        { 
+            if((mask & data[idxOf1]) == 0) break;
+        }
+        if (idxOf0 > endIdx || idxOf1 < startIdx || idxOf0 > idxOf1)
+        {
+            idxOf0--;
+            idxOf1++;
+            break;
+        }
+        else
+        {
+            tmpData = data[idxOf0];
+            data[idxOf0] = data[idxOf1];
+            data[idxOf1] = tmpData;
+        }
+    }
+    int startIdxOf0 = startIdx, endIdxOf0 = idxOf0;
+    int startIdxOf1 = idxOf1, endIdxOf1 = endIdx;
+    if (digit == 1) 
+    {
+        return 0;
+    }
+    else 
+    {
+       radixSort(data, endIdxOf0-startIdxOf0+1, digit-1, startIdxOf0, endIdxOf0);
+       radixSort(data, endIdxOf1-startIdxOf1+1, digit-1, startIdxOf1, endIdxOf1);
+    }
+    print4bit(data, n);
+    return 0;
+}
--- algorithm-radix-sort/main.c
+++ algorithm-radix-sort/main.c
@@ -6,13 +6,32 @@ void print(int arr[], int n)
     {
         printf("%d ", arr[i]);
     }
+    printf("\n");
 }
+
+void print4bit(int arr[], int n) 
+{
+    int bitmask1 = 1;
+    int bitmask2 = 2;
+    int bitmask4 = 4;
+    int bitmask8 = 8;
+    printf ("------\n");
+    for (register int r = 0; r<n; r++)
+    {
+        printf("%d%d%d%d", (arr[r]&bitmask8)>>3, (arr[r]&bitmask4)>>2, (arr[r]&bitmask2)>>1, arr[r]&bitmask1);
+        printf("\n");
+    }
+    printf ("------\n");
+}
+
+extern int radixSort(int data[], int n, int digit, int startIdx, int endIdx);
 
 int main()
 {
-    int arr[10] = {0};
+    int arr[10] = {9, 8, 7, 4, 3, 1, 2, 10, 5, 6};
     int n = sizeof(arr)/sizeof(arr[0]);
     print(arr, n);
-    printf("Hello world!\n");
+    radixSort(arr, n, 4, 0, n-1);
+    print(arr, n);
     return 0;
 }
